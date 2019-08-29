3ee0fb5f09200e3e7fcb66e495a90411a633575d
Mahesh C. Regmi <36955694+geekyarthurs@users.noreply.github.com>
2019-08-30 01:14:38 +0545

    Added Express Support

--- blockchain.js
+++ blockchain.js
@@ -1,5 +1,11 @@
-
 const sha256 = require('js-sha256').sha256;
+var express = require('express')
+var bodyParser = require('body-parser');
+var app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 class Block {
     constructor(index, data, previousHash, proofOfWork) {
         this.index = index;
@@ -11,14 +17,11 @@ class Block {
 
 
     }
-
-
-
     getHash() {
 
-       return sha256.create().update(JSON.stringify(this.data) + this.previousHash + this.index + this.timestamp).hex();
+        return sha256.create().update(JSON.stringify(this.data) + this.previousHash + this.index + this.timestamp).hex();
 
-    
+
     }
 
 
@@ -26,34 +29,34 @@ class Block {
 
 class BlockChain {
     constructor() {
-        
+
         this.chain = [];
-        
-        
+
+
     }
 
-    proofOfWork(previousProof){
+    proofOfWork(previousProof) {
 
         console.log(previousProof)
 
         var proof = 1;
         var correctProof = false;
-        
-        while(correctProof == false){
+
+        while (correctProof == false) {
             // console.log(proof)
-           
-          var hash = sha256.create().update((proof**2 - previousProof**2).toString()).hex().toString();
-           console.log(hash)
-            
-            if(hash.slice(0,3) == '000'){
+
+            var hash = sha256.create().update((proof ** 2 - previousProof ** 2).toString()).hex().toString();
+
+
+            if (hash.slice(0, 3) == '000') {
                 correctProof = true;
-            }else{
+            } else {
                 proof += 1;
-                
+
             }
 
 
-            
+
 
         }
 
@@ -61,14 +64,16 @@ class BlockChain {
 
     }
 
-//533 1
+    //533 1
     addBlock(data) {
-        
-        var proofOfWork = this.chain.length !== 0 ?  this.proofOfWork(this.chain[this.chain.length - 1].proofOfWork) : 1;
+
+        var proofOfWork = this.chain.length !== 0 ? this.proofOfWork(this.chain[this.chain.length - 1].proofOfWork) : 1;
         var index = this.chain.length;
         var previousHash = this.chain.length !== 0 ? this.chain[this.chain.length - 1].hash : 0;
-        var block = new Block(index, data, previousHash,proofOfWork)
+        var block = new Block(index, data, previousHash, proofOfWork)
         this.chain.push(block)
+
+        return block;
     }
 
     chainIsValid() {
@@ -77,7 +82,7 @@ class BlockChain {
                 return false
             }
 
-            if ( i > 0 && this.chain[i].previousHash !== this.chain[i-1].hash){
+            if (i > 0 && this.chain[i].previousHash !== this.chain[i - 1].hash) {
                 return false;
             }
 
@@ -87,7 +92,33 @@ class BlockChain {
 }
 
 const Rupaiya = new BlockChain();
-Rupaiya.addBlock({sender: 'Mahesh', receiver : 'Prajin', amount : 1500})
-Rupaiya.addBlock({sender: 'Mahesh', receiver : 'Prajin', amount : 1500})
+
 
 console.log(Rupaiya.chain)
+
+
+app.post('/mine_block', (req, res) => {
+
+
+
+
+    var sender = req.body.sender;
+    var receiver = req.body.receiver;
+    var amount = req.body.amount;
+
+    var obj = Rupaiya.addBlock({
+        "sender": sender,
+        "receiver": receiver,
+        "amount": amount
+    })
+
+    res.send(obj);
+
+
+})
+
+app.get('/get_chain', (req, res) => {
+    res.send(Rupaiya.chain);
+})
+
+app.listen(3000);
